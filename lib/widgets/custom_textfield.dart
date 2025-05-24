19b38845d2be564581dd29db20286c74d55dc924
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-05-24 22:50:01 +0530

    Add hintText parameter to CustomTextfield for improved usability

--- lib/widgets/custom_textfield.dart
+++ lib/widgets/custom_textfield.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 class CustomTextfield extends StatelessWidget {
   final TextEditingController controller;
-  const CustomTextfield({super.key,required this.controller});
+  final String hintText;
+  const CustomTextfield({super.key,required this.controller,required this.hintText});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
       decoration: InputDecoration(
+        hintText: hintText,
         border: OutlineInputBorder(
           borderSide: BorderSide(
             color: Colors.black38,
